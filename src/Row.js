98743ca8e1eedace06f3248d33c0ffce371db488
CharwekarAkshay <charwekar12@gmail.com>
2020-09-10 01:18:22 +0530

    Done with navbar

---
+++ src/Row.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import './Row.css';
+
+const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+
+  // A snippet of code  which runs based on a specific condition.
+  useEffect(() => {
+    /*
+     * If we leave [] than code will execute only once on page load
+     * If we have [movies] whenever this variable will change function will run
+     */
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+
+    fetchData();
+  }, [fetchUrl]);
+
+
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${baseUrl}${isLargeRow ? movie.poster_path : movie.backdrop_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Row;
